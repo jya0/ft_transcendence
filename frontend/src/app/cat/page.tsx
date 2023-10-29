8c761622efd6f97c6e60ca8d033ae81016aad369
Ahmed Hassan <digiator2020@gmail.com>
2023-10-29 22:15:09 +0000

    docker in parent, next auth

---
+++ frontend/src/app/cat/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CatEvolution } from "../components/CatEvolution";
+
+const cat = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <CatEvolution />
+    </main>
+  );
+};
+
+export default cat;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,16 @@
-import { CatEvolution } from "./components/CatEvolution";
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <CatEvolution />
+      <button
+        className="bg-white w-32 h-32 text-3xl hover:bg-red-500"
+        onClick={() => router.push("/cat")}
+      >
+        cat
+      </button>
     </main>
   );
 }
