59b68fcfcd2340e3d3f20cd14911348dbbb3c456
Ahmed Hassan <digiator2020@gmail.com>
2023-10-30 14:12:02 +0400

    middleware

--- frontend/src/app/api/auth/[...nextauth]/options.ts
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -1,12 +1,19 @@
-import type { NextAuthOptions } from 'next-auth'
-import GitHubProvider from 'next-auth/providers/github'
+import type { NextAuthOptions, Session, DefaultSession } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
 export const options: NextAuthOptions = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-    ],
-	secret: process.env.NEXTAUTH_SECRET as string,
-}
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET as string,
+  callbacks: {
+    async session({ session, token, user, newSession }) {
+      const { name, email, image } = user;
+      session.user = { name, email, image };
+      return session;
+    },
+  },
+};
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,18 @@
-"use client";
-import { useRouter } from "next/navigation";
+import UserCard from "./components/UserCard";
+import { options } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth/next";
 
-export default function Home() {
-  const router = useRouter();
+export default async function Home() {
+  const session = await getServerSession(options);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <button
+      <UserCard user={session?.user} pagetype={"Home"} />
+      {/* <button
         className="bg-white w-32 h-32 text-3xl hover:bg-red-500"
-        onClick={() => router.push("/cat")}
       >
         cat
-      </button>
+      </button> */}
     </main>
   );
 }
