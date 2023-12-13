5258e963503343a68f2cf784b941f3e28dd708bc
Ahmed Hassan <digiator2020@gmail.com>
2023-12-13 17:34:54 +0400

    addin login form, register form

--- Backend/authentication/models.py
+++ Backend/authentication/models.py
@@ -9,10 +9,10 @@ from datetime import datetime
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=50, unique=True, default="None")
-    display_name = models.CharField(max_length=50, unique=True)
+    display_name = models.CharField(max_length=50, unique=False)
     first_name = models.CharField(max_length=50, default="None")
     last_name = models.CharField(max_length=50, default="None")
-    email = models.TextField()
+    email = models.TextField(unique=True, default="None")
     avatar = models.TextField(default="None")
     picture = models.CharField(max_length=1000, default="None")
     is_active = models.BooleanField(default=True)
--- Backend/authentication/urls.py
+++ Backend/authentication/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from authentication.views import auth, logout, get_user_data
+from authentication.views import auth, logout, get_user_data, twoFactorView, login_view, register_view
 
 urlpatterns = [
     path("auth/", auth, name="auth"),
     path("admin/", admin.site.urls),
     path('logout/', logout, name='logged_out'),
-    path('get_user_data/', get_user_data, name='get_user_data')
+    path('get_user_data/', get_user_data, name='get_user_data'),
+    path('2fa/', twoFactorView, name='2fa'),
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
 ]
