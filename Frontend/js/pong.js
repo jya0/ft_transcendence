a687b147d9b7a93662286c6f1af2d21097103005
Radi Aman Riyas <radiriyas@gmail.com>
2024-01-16 23:03:59 +0400

    feat: implement client side socket handlers for online multiplayer

--- Frontend/js/pong.js
+++ Frontend/js/pong.js
@@ -1,6 +1,7 @@
-
+// import {io} from "socket.io-client";
 export const loadGame = () => {
 
+	let btnCounter = 0;
 	const canvas = document.getElementById('pongCanvas');
 	const ctx = canvas.getContext('2d');
 	const startButton = document.getElementById('startButton');
@@ -15,6 +16,10 @@ export const loadGame = () => {
 	const keys = {};
 	let isGameOver = false;
 	let animationFrameId;
+
+	let socketStatus = false;
+
+	let keyPressed;
 
 	function draw() {
 		ctx.fillStyle = '#000';
@@ -43,11 +48,27 @@ export const loadGame = () => {
 			ball.x > canvas.width - paddle.width && ball.y > players.right && ball.y < players.right + paddle.height) {
 			ball.speedX *= -1;
 		}
-
-		if (keys['ArrowUp'] && players.right > 0) players.right -= paddle.speed;
-		if (keys['ArrowDown'] && players.right < canvas.height - paddle.height) players.right += paddle.speed;
-		if (keys['w'] && players.left > 0) players.left -= paddle.speed;
-		if (keys['s'] && players.left < canvas.height - paddle.height) players.left += paddle.speed;
+		
+		if (keys['w'] && players.left > 0){
+			players.left -= paddle.speed;
+			keyPressed = 'w';
+			chatSocket.send(JSON.stringify({
+				'type':'update',
+				'username': localStorage.getItem('username'),
+				'key':keyPressed
+			}))
+		}
+		if (keys['s'] && players.left < canvas.height - paddle.height) {
+			players.left += paddle.speed;
+			keyPressed = 's';
+			chatSocket.send(JSON.stringify({
+				'type':'update',
+				'username': localStorage.getItem('username'),
+				'key': keyPressed
+			}))
+		}
+		// if (keys['w'] && players.left > 0) players.left -= paddle.speed;
+		// if (keys['s'] && players.left < canvas.height - paddle.height) players.left += paddle.speed;
 
 		if (ball.x < 0 || ball.x > canvas.width) {
 			ball.x > canvas.width ? score.left++ : score.right++;
@@ -79,12 +100,102 @@ export const loadGame = () => {
 		keys[event.key] = false;
 	});
 
+
+
+
+
+	let player_count = 0;
+	let url = `ws://10.12.4.7:8000/ws/socket-server/`
+
+	const chatSocket = new WebSocket(url)
+
+	chatSocket.addEventListener("open", (event) => {
+		if (socketStatus == true)
+			return
+		socketStatus = true;
+
+		chatSocket.send(JSON.stringify({
+			'type':'start',
+			'username': localStorage.getItem('username')
+		}))
+		
+		player_count = 1;
+
+		console.log("waiting for a second player...")
+	});
+
+	// chatSocket.addEventListener("error", (errorEvent) => {
+	// 	console.error("WebSocket error:", errorEvent);
+	// 	// Handle the error gracefully here
+	// 	// Set gameOver to true or take appropriate actions
+	// 	// gameOver = true;
+	//   });
+	  
+	//   chatSocket.addEventListener("close", (closeEvent) => {
+	// 	console.log("WebSocket closed:", closeEvent);
+	// 	// Handle the WebSocket close event here
+	// 	// Set gameOver to true or take appropriate actions
+	// 	// gameOver = true;
+	//   });
+	  
+	 
+	//   window.addEventListener("beforeunload", function (e) {
+	// 	var confirmationMessage = "\AYYOO WHAT AR U DOING U WANNA LOSE?????/";
+	  
+	// 	(e || window.event).returnValue = confirmationMessage; //Gecko + IE
+	// 	chatSocket.send(JSON.stringify({
+	// 		'type':'close',
+	// 		'username': localStorage.getItem('username')
+	// 	}))
+	// 	return confirmationMessage;                            //Webkit, Safari, Chrome
+	//   });
+
+
+
+
+	chatSocket.onmessage = function (e) {
+		let data = JSON.parse(e.data)
+		console.log('Data: ', data)
+
+		if (btnCounter != 0 && data.type === 'start' && data.sender != localStorage.getItem('username')&& data["status"] == "start") {
+			player_count = player_count + 1;
+			// document.getElementById("startButton").innerHTML = "Second player found. Press to begin";
+			startButton.click();
+		}
+		else if(data.type == 'update') {
+			if (data['sender'] == localStorage.getItem('username'))
+				return ;
+			if (data['key']=='w' && players.right > 0) players.right -= paddle.speed;
+			if (data['key'] =='s' && players.right < canvas.height - paddle.height) players.right += paddle.speed;
+		}
+		else if(data.type == 'close') {
+			gameOver = true;
+			player_count = 1;
+			chatSocket.close();
+		}
+		else {
+			console.log("woops not yet...")
+		}
+	}
+	startButton.click();
 	startButton.addEventListener('click', () => {
+		btnCounter = btnCounter + 1;
+
+		if (player_count == 1) {
+			// document.getElementById("startButton").innerHTML = "Waiting for second player ..."
+			return;
+		}
+
 		if (isGameOver || !animationFrameId) {
 			isGameOver = false;
 			score.left = 0;
 			score.right = 0;
 			resetBall();
+
+			// if (player_count == 1) {
+			// 	console.log("still waiting for a second player...")
+			// 	return;
+			// }
 			animationFrameId = requestAnimationFrame(gameLoop);
 			startButton.disabled = true;
 		}
