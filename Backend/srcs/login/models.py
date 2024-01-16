29b8db59606c9d21ca1c7a93d59c3c446be22767
Radi Aman Riyas <radiriyas@gmail.com>
2024-01-16 23:04:45 +0400

    feat: integrate backend websocket handling with frontend

--- Backend/srcs/login/models.py
+++ Backend/srcs/login/models.py
@@ -35,6 +35,7 @@ class UserProfile(AbstractUser):
     groups = models.ManyToManyField(Group, related_name='user_profiles')
     id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True)
+    intra = models.CharField(max_length=50, default="default")
     display_name = models.CharField(max_length=50, unique=False)
     avatar = models.TextField(default="None")
     picture = JSONField(default=dict)
@@ -53,42 +54,45 @@ class UserProfile(AbstractUser):
 class Settings(models.Model):
     avatar = models.TextField(default="None")
     display_name = models.CharField(max_length=50, unique=True, primary_key=True)
-    intra = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    id = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     is_2fa_enabled = models.BooleanField(default=False)
     def __str__(self):
         return self.display_name
 
 
 #no primary key - try to create a composite key
-class Friendship(models.Model):
-    intra1 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='user1')
-    intra2 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='user2')
-    def __str__(self):
-        return (self.intra1 + self.intra2)
+# class Friendship(models.Model):
+#     id1 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='user1')
+#     id2 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='user2')
+#     def __str__(self):
+#         return (self.id1 + "-" + self.id2)
 
 
 class Tournament(models.Model):
-    intra = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    winner = models.CharField(max_length=50, unique=False, default="1")
     tournament_id = models.AutoField(primary_key=True)
     status = models.BooleanField(default=False)
-    def __str__(self):
-        return self.intra
+    # def __str__(self):
+    #     return self.tournament_id + "" 
 
 class Match(models.Model):
+    open_lobby = models.BooleanField(default=True)
     match_id = models.AutoField(primary_key=True)
     tournament_id = models.ForeignKey(Tournament, on_delete=models.CASCADE)
-    intra1 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='player1')
-    intra2 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='player2')
+    id1 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='player1')
+    id2 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='player2')
     winner = models.CharField(max_length=50)
     score1 = models.IntegerField()
     score2 = models.IntegerField()
-    def __str__(self):
-        return self.match_id
+    ongoing = models.BooleanField(default=False)
+    # def __str__(self):
+    #     return self.match_id
     
-#no primary key - try to create a composite key
-class Nickname(models.Model):
-    nick = models.CharField(max_length=50)
-    intra = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING)
-    tournament_id = models.ForeignKey(Tournament, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.nick
+# #no primary key - try to create a composite key
+# class Nickname(models.Model):
+#     nid = models.AutoField(primary_key=True)
+#     nick = models.CharField(max_length=50)
+#     id = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING)
+#     tournament_id = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+#     def __str__(self):
+#         return self.nick
