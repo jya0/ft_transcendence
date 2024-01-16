be39db46f68fe4f9e1bcc47640db2c0ef60d1308
Radi Aman Riyas <radiriyas@gmail.com>
2024-01-16 23:05:22 +0400

    feat: setup ASGI to accept WebSocket protocol

--- Backend/srcs/pong_os/asgi.py
+++ Backend/srcs/pong_os/asgi.py
@@ -7,10 +7,31 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pong_os.settings')
+
+# application = get_asgi_application()
+
+
+
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pong_os.settings')
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import game.routing
 
-application = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
+
+application = ProtocolTypeRouter({
+    'http' : get_asgi_application(),
+    'websocket' : AuthMiddlewareStack(
+        URLRouter(
+            game.routing.websocket_urlpatterns
+        )
+    )
+})
