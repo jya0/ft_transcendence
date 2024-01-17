d7be35ddf9333f171120c52a78f91cb19fe3e6d2
Radi Aman Riyas <radiriyas@gmail.com>
2024-01-17 18:42:44 +0400

    feat: introduce friendships & nicknames

--- Backend/srcs/login/models.py
+++ Backend/srcs/login/models.py
@@ -3,33 +3,6 @@ from django.contrib.auth.models import BaseUserManager, AbstractUser
 from django.db.models import JSONField
 from django.contrib.auth.models import Group, Permission
 
-# Create your models here.
-# class UserProfile(AbstractUser):
-#     id = models.AutoField(primary_key=True)
-#     email = models.EmailField(unique=True)
-#     display_name = models.CharField(max_length=50, unique=False)
-#     is_2fa_enabled = models.BooleanField(default=False)
-#     is_online = models.BooleanField(default=False)
-
-#     # USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['email']
-
-#     def __str__(self):
-#         return self.display_name
-    
-
-# class UserProfile(AbstractUser):
-#     id = models.AutoField(primary_key=True, default=0)
-#     intra = models.TextField(default="None")
-#     email = models.EmailField(unique=True)
-#     password = models.CharField(max_length=50, unique=False)
-#     is_online = models.BooleanField(default=False)
-#     date_joined = models.DateTimeField() #get current date time
-#     last_login = models.DateTimeField() #get current date time
-#     display_name = models.CharField(max_length=50, unique=False)
-#     picture = JSONField(default=dict)
-#     def __str__(self):
-#         return self.intra
 
 class UserProfile(AbstractUser):
     groups = models.ManyToManyField(Group, related_name='user_profiles')
@@ -60,20 +33,17 @@ class Settings(models.Model):
         return self.display_name
 
 
-#no primary key - try to create a composite key
-# class Friendship(models.Model):
-#     id1 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='user1')
-#     id2 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='user2')
-#     def __str__(self):
-#         return (self.id1 + "-" + self.id2)
+class Friendship(models.Model):
+    id1 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='user1')
+    id2 = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, related_name='user2')
+
 
 
 class Tournament(models.Model):
     winner = models.CharField(max_length=50, unique=False, default="1")
     tournament_id = models.AutoField(primary_key=True)
     status = models.BooleanField(default=False)
-    # def __str__(self):
-    #     return self.tournament_id + "" 
+
 
 class Match(models.Model):
     open_lobby = models.BooleanField(default=True)
@@ -85,14 +55,10 @@ class Match(models.Model):
     score1 = models.IntegerField()
     score2 = models.IntegerField()
     ongoing = models.BooleanField(default=False)
-    # def __str__(self):
-    #     return self.match_id
+
     
-# #no primary key - try to create a composite key
-# class Nickname(models.Model):
-#     nid = models.AutoField(primary_key=True)
-#     nick = models.CharField(max_length=50)
-#     id = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING)
-#     tournament_id = models.ForeignKey(Tournament, on_delete=models.CASCADE)
-#     def __str__(self):
-#         return self.nick
+class Nickname(models.Model):
+    nid = models.AutoField(primary_key=True)
+    nick = models.CharField(max_length=50)
+    id = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING)
+    tournament_id = models.ForeignKey(Tournament, on_delete=models.CASCADE)
