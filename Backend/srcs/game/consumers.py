59696cccd8457c65cd60ef8cefed035ed99683de
Radi Aman Riyas <radiriyas@gmail.com>
2024-01-20 09:36:57 +0400

    feat: online tourn db updates for matchmaking

--- Backend/srcs/game/consumers.py
+++ Backend/srcs/game/consumers.py
@@ -10,8 +10,30 @@ def create_new_game_lobby():
     dummy = Tournament.objects.all()[0]
     Match.objects.create(tournament_id_id=dummy.tournament_id, id1_id=2, id2_id=3, score1=0, score2=0, ongoing=False)
 
+def prepare_final_round(tourn, user):
+    ready = False
+    game = Match.objects.create(tournament_id_id=tourn.tournament_id, id1_id=2, id2_id=3, score1=0, score2=0, ongoing=True)
+
+    print('Okay so these are the games i got for the final round: ')
+
+    if (game.id1.intra =='temp1'):
+        g = Match.objects.get(match_id=game.match_id)
+        g.id1 = user
+        g.save()
+        ready = False
+    elif (game.id1.intra == 'temp2'):
+        g = Match.objects.get(match_id=game.match_id)
+        g.id1 = user
+        g.save()
+        ready = True
+    game.save()
+    return ready
+
+
 class GameConsumer(WebsocketConsumer):
     lobbyCount = 0
+
+
 
     def connect(self):
         
@@ -26,97 +48,153 @@ class GameConsumer(WebsocketConsumer):
         )
         print('sup mfs')
         self.accept()
-       
-        # self.send(text_data=json.dumps({
-        #     'type': 'connection established',
-        #     'start' : start
-        # }))
-        # async_to_sync(self.channel_layer.group_send)(
-        #     self.room_group_name,
-        #     {
-        #         'type': 'start',
-        #         'username' : start
-        #     }
-        # )
-
 
     def receive(self, text_data):
+
         text_data_json = json.loads(text_data)
         print('receieved from client: ', text_data_json)
         type = text_data_json['type']
+        mode = text_data_json['mode']
         username = text_data_json['username']
-
-        #handle new game request:
-        if (type == 'start'):
-            status = 'waiting'
-            dummy = Tournament.objects.all()[0]
-            open_lobby = Match.objects.filter(Q(open_lobby=True)& Q(tournament_id_id=dummy)).exists()
-            print(open_lobby)
-            game =  Match.objects.filter(Q(open_lobby=True) & Q(tournament_id_id=dummy))[0]
-            player = UserProfile.objects.filter(Q(intra=username)).all()[0]
-            if (open_lobby == True):
-                #case 1: no players yet
-                if (game.id1.intra == 'default'):
-                    print("looks like ur the first player!!!!")
-                    # self.room_group_name = username
-                    game.id1 = player
-                #case 2: lobby half full
-                else :
-                    if (game.id1 != player):
-                        print("we found u a match!!!!!!!")
-                        # self.room_group_name = game.id1.intra
-                        game.id2 = player
-                        status = 'start'
-                        game.open_lobby = False
-                        game.ongoing = True
-                        create_new_game_lobby()
-                game.save()
-                #@todo:
-                #if game full, create a new one now:
-
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'start_game',
-                    'sender' : username,
-                    'status' : status
-                }
-            )
-
-
+        
+        #handle in-game player movements
         if (type == 'update'):
             key = text_data_json["key"]
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name,
                 {
                     'type': 'update_game',
+                    'mode' : mode,
                     'sender': username,
                     'key' : key,
                 }
             )
+        
+        
+        if (mode == 'tournament'):
+            print(text_data_json)
+            t_name = text_data_json['tournament_name']
+            #handle new game request:
+            if (type == 'start'):
+                status = 'waiting'
+                tourn = Tournament.objects.get(name=t_name)
+                games =  Match.objects.filter(Q(tournament_id=tourn.tournament_id))
+                # print('Here are the games for this tournament boys (should be 2 pls): ')
+                # print(games[0].__dict__)
+                player = UserProfile.objects.get(intra=username)
+                lobbyFull = True
+                game = games[0]
+                # print("so: id1.intra = " + game.id1.intra + " and game.id2.intra=" + game.id2.intra)
+                if (game.id1.intra == 'temp1' or game.id2.intra == 'temp2'):
+                    lobbyFull = False
+                # game = games[1]
+                # if (game.id1.intra == 'temp1' or game.id2.intra == 'temp2'):
+                #     lobbyFull = False
+                if (lobbyFull):
+                    status = 'start'
+                async_to_sync(self.channel_layer.group_send)(
+                    self.room_group_name,
+                    {
+                        'type': 'start_game',
+                        'mode' : mode,
+                        'sender' : username,
+                        'status' : status
+                    }
+                )
 
+            #handle game ends
+            if (type == 'end'):
+                tourn = Tournament.objects.get(name=t_name)
+                games =  Match.objects.filter(Q(ongoing=True) & Q(tournament_id=tourn.tournament_id))
+                player = UserProfile.objects.get(intra=username)
+                score1 = text_data_json['score1']
+                score2 = text_data_json['score2']
+                current_game.ongoing = False
+                if (score1 > score2):
+                    current_game.winner = current_game.id1.intra
+                else:
+                    current_game.winner = current_game.id2.intra
+                current_game.score1 = score1
+                current_game.score2 = score2
+                current_game.save()
 
-        if (type == 'end'):
-            current_game = Match.objects.filter((Q(id1__intra=username)|Q(id2__intra=username)) & Q(ongoing=True))[0]
-            player = UserProfile.objects.filter(Q(intra=username))[0]
-            score1 = text_data_json['score1']
-            score2 = text_data_json['score2']
-            current_game.ongoing = False
-            if (score1 > score2):
-                current_game.winner = current_game.id1.intra
-            else:
-                current_game.winner = current_game.id2.intra
-            current_game.score1 = score1
-            current_game.score2 = score2
-            current_game.save()
-            self.close()
+                ready = prepare_final_round(tourn, player)
+                status = 'waiting'
+                if (ready):
+                    status = 'start'
+                async_to_sync(self.channel_layer.group_send)(
+                    self.room_group_name,
+                    {
+                        'type': 'start_game',
+                        'mode' : mode,
+                        'sender' : username,
+                        'status' : status
+                    }
+                )
+
+        else:
+            #handle new game request:
+            if (type == 'start'):
+                status = 'waiting'
+                dummy = Tournament.objects.all()[0]
+                open_lobby = Match.objects.filter(Q(open_lobby=True)& Q(tournament_id_id=dummy)).exists()
+                print(open_lobby)
+                game =  Match.objects.filter(Q(open_lobby=True) & Q(tournament_id_id=dummy))[0]
+                player = UserProfile.objects.filter(Q(intra=username)).all()[0]
+                if (open_lobby == True):
+                    #case 1: no players yet
+                    if (game.id1.intra == 'temp1'):
+                        print("looks like ur the first player!!!!")
+                        # self.room_group_name = username
+                        game.id1 = player
+                    #case 2: lobby half full
+                    else :
+                        if (game.id1 != player):
+                            print("we found u a match!!!!!!!")
+                            # self.room_group_name = game.id1.intra
+                            game.id2 = player
+                            status = 'start'
+                            game.open_lobby = False
+                            game.ongoing = True
+                            create_new_game_lobby()
+                    game.save()
+                    #@todo:
+                    #if game full, create a new one now:
+
+                async_to_sync(self.channel_layer.group_send)(
+                    self.room_group_name,
+                    {
+                        'type': 'start_game',
+                        'mode' : mode,
+                        'sender' : username,
+                        'status' : status
+                    }
+                )
+
+            #handle game ends
+            if (type == 'end'):
+                current_game = Match.objects.filter((Q(id1__intra=username)|Q(id2__intra=username)) & Q(ongoing=True))[0]
+                player = UserProfile.objects.filter(Q(intra=username))[0]
+                score1 = text_data_json['score1']
+                score2 = text_data_json['score2']
+                current_game.ongoing = False
+                if (score1 > score2):
+                    current_game.winner = current_game.id1.intra
+                else:
+                    current_game.winner = current_game.id2.intra
+                current_game.score1 = score1
+                current_game.score2 = score2
+                current_game.save()
+                self.close()
 
 
     def start_game(self, event):
         message = event['status']
         sender = event['sender']
+        mode = event['mode']
         self.send(text_data=json.dumps({
             'type' : 'start',
+            'mode' : mode,
             'sender' : sender,
             'status' : message
         }))
@@ -124,84 +202,19 @@ class GameConsumer(WebsocketConsumer):
     def update_game(self, event):
         message = event['key']
         sender = event['sender']
+        mode = event['mode']
         self.send(text_data=json.dumps({
                     'type' : 'update',
+                    'mode' : mode,
                     'sender': sender,
                     'key' : message
                 }))
 
-
     def disconnect(self, code):
-        # self.send(text_data=json.dumps({
-        #     'type': 'close',
-        # }))
 
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
+        print('closing socket bruv')
         self.close()
-        # is_playing = Match.objects.filter((Q(id1__id='rriyas') | Q(id2__id='rriyas')) & ongoing=True).exists()
-        
-        # open_games = Match.objects.all().count()
-        #Case 1:  match to an open game
-        # if (open_games == 0):
-        #      Match.objects.create(
-        #             tournament_id=-1,
-        #             open_lobby=True,
-        #             id1=username
-        #         )
-
-
-
-        # #Case 2: hes not in a game AND hes the first player
-        # # in_game = Match.objects.filter((Q(id1__intra=username) | Q(id2__intra=username)) & Q(ongoing=True)).exists()
-        # in_game = Match.objects.filter(
-        #     (Q(id1__display_name=username) | Q(id2__display_name=username)) & Q(ongoing=True)
-        # ).exists()
-
-        # status = 'waiting'
-        # if (in_game == False):
-            
-        #     #look for open games
-        #     open_games = Match.objects.filter(Q(open_lobby=True)).exists()
-        #     #Case 1:  match to an open game
-        #     if (open_games == True):
-        #         Match.objects.filter(Q(open_lobby=True)).first().id2.display_name = username
-        #         status = 'start'
-
-        #     #Case 2: create a new open game 
-        #     else:
-        #         Match.objects.create(
-        #             tournament_id=-1,
-        #             open_lobby=True,
-        #             id1=username
-        #         )
-        #         status = 'waiting'
-
-
-        #     async_to_sync(self.channel_layer.group_send)(
-        #         self.room_group_name,
-        #         {
-        #             'type': 'start_game',
-        #             'status' : status
-        #         }
-        #     )
-
-
-        #Case 2: hes in a game already - so record movement
-        # is_playing = Match.objects.filter((Q(id1__id=username) | Q(id2__id=username)) & Q(ongoing=True)).exists()
-        # if (is_playing == True):
-            
-        # broadcast a mesg to those in this group
-        # async_to_sync(self.channel_layer.group_send)(
-        #     self.room_group_name,
-        #     {
-        #         'type' : 'send_pos_data',
-        #         'new_pos' : message
-        #     }
-        # )
-        # self.send(text_data=json.dumps({
-        #     'type' : 'chat',
-        #     'message' : message
-        # }))
