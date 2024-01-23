9cbf549863854b41eeebefae6eaac30ed5f461b5
Ahmed Hassan <digiator2020@gmail.com>
2024-01-23 21:14:42 +0400

    frontend auth done

--- Backend/srcs/login/utils.py
+++ Backend/srcs/login/utils.py
@@ -13,8 +13,10 @@ import os
 import requests
 
 
-def send_otp(request):
-    user = get_object_or_404(UserProfile, username=request.user.username)
+def send_otp(request, username):
+    print("send_otp view")
+    user = get_object_or_404(UserProfile, username=username)
+    print("user:", user)
     totp = pyotp.TOTP(pyotp.random_base32(), interval=60)
     otp = totp.now()
     request.session['otp_secret_key'] = otp
