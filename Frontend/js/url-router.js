80974a66ffa86562473a7dc0ea98f6c8156ef355
Ahmed Hassan <digiator2020@gmail.com>
2024-01-23 14:40:33 +0400

    auth in frontend, takes time

--- Frontend/js/url-router.js
+++ Frontend/js/url-router.js
@@ -12,6 +12,7 @@ let email
 let userToken
 let userId
 let user = localStorage.getItem('user');
+let loginIn = false;
 
 if (user) {
 	user = JSON.parse(user)
@@ -176,9 +177,11 @@ let loadFile = function (event) {
 };
 
 const urlLocationHandler = async () => {
-	if (tokenHandler()) {
-		return;
-	}
+	// if (tokenHandler()) {
+	// 	return;
+	// }
+
+
 	insertOrCreateContent();
 	document.getElementById("content").innerHTML = ``;
 	let location = window.location.pathname;
@@ -188,22 +191,30 @@ const urlLocationHandler = async () => {
 	if (location.length == 0) {
 		location = "/";
 	}
+	console.log('after login -> ', location);
+	console.log('after login -> ', localStorage.getItem('access_token'));
 	if (location === '/' && localStorage.getItem('access_token')) {
+		console.log('desktop route');
 		location = '/desktop';
 	}
 	if (!localStorage.getItem('access_token')) {
+		console.log('no access route');
 		location = '/';
 	}
 	const route = urlRoutes[location] || urlRoutes["404"];
 
 	if (location === '/') {
+		console.log('login route');
 		if (document.getElementById("navbar")) {
 			document.getElementById("navbar").remove();
 		}
 		await fetch('/components/login.html').then(response => response.text()).then(data => {
 			document.getElementById("content").innerHTML = data;
 		});
+		return;
 	}
+	document.getElementById("navbar").style.display = 'flex';
+
 	if (location === '/play') {
 		setMainWindowframe();
 		if (!document.getElementById("pongCanvas")) {
@@ -528,133 +539,286 @@ const urlLocationHandler = async () => {
 };
 
 // add an event listener to the window that watches for url changes
-window.onpopstate = urlLocationHandler;
-// call the urlLocationHandler function to handle the initial url
-window.route = urlRoute;
-// call the urlLocationHandler function to handle the initial url
-urlLocationHandler();
 
-function tokenHandler() {
+// call the urlLocationHandler function to handle the initial url
+handleUserData();
+
+async function handleUserData() {
+
+
 	const urlParams = new URLSearchParams(window.location.search);
-	const token = urlParams.get('token');
-	const otp = urlParams.get('otp');
-	const urlUsername = urlParams.get('username');
-	console.log(token);
-	console.log(otp);
-	if (token && !otp) {
-		console.log('token---------->');
-		localStorage.setItem('access_token', token);
-		let userData;
-		fetch('http://localhost:8000/api/get_user_data/', {
+	const code = urlParams.get('code');
+	const url = new URL(window.location.href);
+	url.search = '';
+	const mainUrl = url.toString();
+
+	history.replaceState({}, '', mainUrl);
+	var FORTY_TWO_CLIENT_ID = "u-s4t2ud-69155ca3ecf1f57fa6e8660a9988bbdd7f03a45128ea80a454d6f13939c4bca5"
+	var FORTY_TWO_CLIENT_SECRET = "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"
+	var FORTY_TWO_REDIRECT_URI = "https://localhost:8090/"
+	console.log('code', code)
+	if (code) {
+		loginIn = true;
+		document.getElementById("content").innerHTML = `
+		<div id="spinner" class="d-flex justify-content-center" style="z-index: 15; top: 50%; color: white; margin-top: 50%;">
+		<div class="spinner-border" role="status" style="width: 250px; height: 250px;">
+		<span class="visually-hidden">Loading...</span>
+		</div>
+		<h1>login you in...</h1>
+		</div>
+		`;
+		// document.getElementById("nav-container").classList.add("hidden");
+		if (document.getElementById("navbar")) {
+			document.getElementById("navbar").style.display = 'none';
+		}
+		console.log("starting fetching....");
+		await fetch(`http://localhost:8000/auth/?code=${code}`, {
 			method: 'GET',
 			headers: {
-				'Authorization': `Bearer ${token}`,
-				'Content-Type': 'application/json'
+				'Content-Type': 'application/json',
 			},
 		})
 			.then(response => {
 				if (!response.ok) {
 					response.statusText === 'Unauthorized' ? alert('Unauthorized') : alert('Network response was not ok');
-				}
-				return response.json();
-			})
-			.then(data => {
-				userData = data[0];
-				if (!userData) {
 					return;
 				}
-				localStorage.setItem('username', userData['username']);
-				localStorage.setItem('user', JSON.stringify(userData));
-				username = userData['username'];
-				image = userData['image'];
-				is_2fa_enabled = userData['is_2fa_enabled'];
-				is_online = userData['is_online'];
-				picture = userData['picture'];
-				email = userData['email'];
-				userId = userData['id'];
-				userToken = token;
+				return response.json();
+			}).then(data => {
+
+				console.log('data', data)
+				if (data.message) {
+					console.log('message', message)
+					return;
+				}
+				userToken = data.token;
+				user = data.user;
+				let csrfToken = data.csrfToken
+				localStorage.setItem('access_token', userToken);
+				localStorage.setItem('username', user.username);
+				console.log(userToken);
+				console.log(user);
+				console.log(csrfToken);
+				console.log(data.sessionId);
+				// document.cookie = "sessionId=" + data.sessionId;
+				// console.log('authDone', authDone)
+
+				window.history.pushState({}, "", '/desktop');
+
+				window.onpopstate = urlLocationHandler;
+				// call the urlLocationHandler function to handle the initial url
+				window.route = urlRoute;
+				urlLocationHandler();
+
+				loginIn = false;
+
+
+				const otp = data.otp
+				console.log('otp', otp)
+				if (otp === 'validate_otp') {
+					console.log('validate otp');
+					// setMainWindowframe();
+					document.getElementById("content").innerHTML = `<div class="window-frame" id="main-window">
+				<div class="top-bar">
+				<img class="top-bar-child" alt="" src="./assets/public/rectangle-4.svg" />
+
+				<div class="options">
+				<img class="vector-icon" alt="" src="./assets/public/vector.svg" />
+
+				<img class="dot-grid-icon" alt="" src="./assets/public/dot-grid.svg" />
+				</div>
+				</div>
+				<div class="window"></div>
+				</div>`;
+					document.getElementsByClassName('window')[0].innerHTML = `
+				<div class="mb-3 p-20px">
+				<label for="otp-input" class="form-label">OTP Code</label>
+				<input type="text" class="form-control" id="otp-input" placeholder="Enter OTP code">
+				</div>
+				<button type="submit-otp" id="submit-otp" class="btn btn-primary">Validate OTP</button>
+				`;
+
+					document.getElementById('submit-otp').addEventListener('click', async () => {
+						let otp = document.getElementById('otp-input').value;
+						if (!otp) {
+							alert('Please enter OTP code');
+							return;
+						}
+						const requestBody = new URLSearchParams();
+						requestBody.append('username', urlUsername);
+						requestBody.append('otp', otp);
+
+						await fetch('http://localhost:8000/validate_otp/', {
+							method: 'POST',
+							headers: {
+								'Content-Type': 'application/x-www-form-urlencoded',
+								'Authorization': `Bearer ${token}`,
+								'x-csrftoken': getCookie('csrftoken'),
+							},
+							body: requestBody.toString(),
+						})
+							.then(response => {
+								if (!response.ok) {
+									throw new Error(response.statusText === 'Unauthorized' ? 'Unauthorized' : 'Network response was not ok');
+								}
+								return response.json();
+							})
+							.then(data => {
+								// Handle the response data here
+								if (data.message === 'OTP is valid') {
+									console.log(data);
+									localStorage.setItem('access_token', token);
+									document.getElementsByClassName("window")[0].innerHTML = '';
+									alert('OTP is valid, enjoy pongos');
+									return false;
+								}
+								else {
+									alert('Invalid OTP code');
+								}
+
+							})
+							.catch(error => {
+								console.error('Error:', error);
+							});
+					});
+				}
+
 			})
-		const url = new URL(window.location.href);
-		url.search = '';
-		const mainUrl = url.toString();
-
-		history.replaceState({}, '', mainUrl);
+		return;
 	}
-	if (otp === 'validate_otp') {
-		console.log('validate otp');
-		// setMainWindowframe();
-		document.getElementById("content").innerHTML = `<div class="window-frame" id="main-window">
-		<div class="top-bar">
-		  <img class="top-bar-child" alt="" src="./assets/public/rectangle-4.svg" />
+	window.onpopstate = urlLocationHandler;
+	// call the urlLocationHandler function to handle the initial url
+	window.route = urlRoute;
+	urlLocationHandler();
 
-		  <div class="options">
-			<img class="vector-icon" alt="" src="./assets/public/vector.svg" />
-
-			<img class="dot-grid-icon" alt="" src="./assets/public/dot-grid.svg" />
-		  </div>
-		  </div>
-			<div class="window"></div>
-		</div>`;
-		document.getElementsByClassName('window')[0].innerHTML = `
-									<div class="mb-3 p-20px">
-									<label for="otp-input" class="form-label">OTP Code</label>
-									<input type="text" class="form-control" id="otp-input" placeholder="Enter OTP code">
-									</div>
-									<button type="submit-otp" id="submit-otp" class="btn btn-primary">Validate OTP</button>
-								`;
-
-		document.getElementById('submit-otp').addEventListener('click', async () => {
-			let otp = document.getElementById('otp-input').value;
-			if (!otp) {
-				alert('Please enter OTP code');
-				return;
-			}
-			const requestBody = new URLSearchParams();
-			requestBody.append('username', urlUsername);
-			requestBody.append('otp', otp);
-
-			await fetch('http://localhost:8000/validate_otp/', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
-					'Authorization': `Bearer ${token}`,
-					'x-csrftoken': getCookie('csrftoken'),
-				},
-				body: requestBody.toString(),
-			})
-				.then(response => {
-					if (!response.ok) {
-						throw new Error(response.statusText === 'Unauthorized' ? 'Unauthorized' : 'Network response was not ok');
-					}
-					return response.json();
-				})
-				.then(data => {
-					// Handle the response data here
-					if (data.message === 'OTP is valid') {
-						console.log(data);
-						localStorage.setItem('access_token', token);
-						document.getElementsByClassName("window")[0].innerHTML = '';
-						alert('OTP is valid, enjoy pongos');
-						return false;
-					}
-					else {
-						alert('Invalid OTP code');
-					}
-
-				})
-				.catch(error => {
-					console.error('Error:', error);
-				});
-		});
-		const url = new URL(window.location.href);
-		url.search = '';
-		const mainUrl = url.toString();
-
-		history.replaceState({}, '', mainUrl);
-		return true;
-	}
-	return false;
 }
+
+
+
+
+
+
+// function tokenHandler() {
+// 	const urlParams = new URLSearchParams(window.location.search);
+// 	const token = urlParams.get('token');
+// 	const otp = urlParams.get('otp');
+// 	const urlUsername = urlParams.get('username');
+// 	console.log(token);
+// 	console.log(otp);
+// 	if (token && !otp) {
+// 		console.log('token---------->');
+// 		localStorage.setItem('access_token', token);
+// 		let userData;
+// 		fetch('http://localhost:8000/api/get_user_data/', {
+// 			method: 'GET',
+// 			headers: {
+// 				'Authorization': `Bearer ${token}`,
+// 				'Content-Type': 'application/json'
+// 			},
+// 		})
+// 			.then(response => {
+// 				if (!response.ok) {
+// 					response.statusText === 'Unauthorized' ? alert('Unauthorized') : alert('Network response was not ok');
+// 				}
+// 				return response.json();
+// 			})
+// 			.then(data => {
+// 				userData = data[0];
+// 				if (!userData) {
+// 					return;
+// 				}
+// 				localStorage.setItem('username', userData['username']);
+// 				localStorage.setItem('user', JSON.stringify(userData));
+// 				username = userData['username'];
+// 				image = userData['image'];
+// 				is_2fa_enabled = userData['is_2fa_enabled'];
+// 				is_online = userData['is_online'];
+// 				picture = userData['picture'];
+// 				email = userData['email'];
+// 				userId = userData['id'];
+// 				userToken = token;
+// 			})
+// 		const url = new URL(window.location.href);
+// 		url.search = '';
+// 		const mainUrl = url.toString();
+
+// 		history.replaceState({}, '', mainUrl);
+// 	}
+// 	if (otp === 'validate_otp') {
+// 		console.log('validate otp');
+// 		// setMainWindowframe();
+// 		document.getElementById("content").innerHTML = `<div class="window-frame" id="main-window">
+// 		<div class="top-bar">
+// 		  <img class="top-bar-child" alt="" src="./assets/public/rectangle-4.svg" />
+
+// 		  <div class="options">
+// 			<img class="vector-icon" alt="" src="./assets/public/vector.svg" />
+
+// 			<img class="dot-grid-icon" alt="" src="./assets/public/dot-grid.svg" />
+// 		  </div>
+// 		  </div>
+// 			<div class="window"></div>
+// 		</div>`;
+// 		document.getElementsByClassName('window')[0].innerHTML = `
+// 									<div class="mb-3 p-20px">
+// 									<label for="otp-input" class="form-label">OTP Code</label>
+// 									<input type="text" class="form-control" id="otp-input" placeholder="Enter OTP code">
+// 									</div>
+// 									<button type="submit-otp" id="submit-otp" class="btn btn-primary">Validate OTP</button>
+// 								`;
+
+// 		document.getElementById('submit-otp').addEventListener('click', async () => {
+// 			let otp = document.getElementById('otp-input').value;
+// 			if (!otp) {
+// 				alert('Please enter OTP code');
+// 				return;
+// 			}
+// 			const requestBody = new URLSearchParams();
+// 			requestBody.append('username', urlUsername);
+// 			requestBody.append('otp', otp);
+
+// 			await fetch('http://localhost:8000/validate_otp/', {
+// 				method: 'POST',
+// 				headers: {
+// 					'Content-Type': 'application/x-www-form-urlencoded',
+// 					'Authorization': `Bearer ${token}`,
+// 					'x-csrftoken': getCookie('csrftoken'),
+// 				},
+// 				body: requestBody.toString(),
+// 			})
+// 				.then(response => {
+// 					if (!response.ok) {
+// 						throw new Error(response.statusText === 'Unauthorized' ? 'Unauthorized' : 'Network response was not ok');
+// 					}
+// 					return response.json();
+// 				})
+// 				.then(data => {
+// 					// Handle the response data here
+// 					if (data.message === 'OTP is valid') {
+// 						console.log(data);
+// 						localStorage.setItem('access_token', token);
+// 						document.getElementsByClassName("window")[0].innerHTML = '';
+// 						alert('OTP is valid, enjoy pongos');
+// 						return false;
+// 					}
+// 					else {
+// 						alert('Invalid OTP code');
+// 					}
+
+// 				})
+// 				.catch(error => {
+// 					console.error('Error:', error);
+// 				});
+// 		});
+// 		const url = new URL(window.location.href);
+// 		url.search = '';
+// 		const mainUrl = url.toString();
+
+// 		history.replaceState({}, '', mainUrl);
+// 		return true;
+// 	}
+// 	return false;
+// }
 
 
 function insertOrCreateContent() {
