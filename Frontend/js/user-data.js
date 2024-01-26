6af35b8a81abc107880fe357b18d052213ac2a74
Ahmed Hassan <digiator2020@gmail.com>
2024-01-26 15:51:38 +0400

    add: yao's time

--- Frontend/js/user-data.js
+++ Frontend/js/user-data.js
@@ -89,3 +89,39 @@ function handleToken() {
 }
 
 
+function updateTime() {
+	const monthNames = ["January", "February", "March", "April", "May", "June",
+						"July", "August", "September", "October", "November", "December"];
+	const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	const now = new Date();
+
+	let year = now.getFullYear();
+	let month = now.getMonth();
+	let dow = now.getDay();
+	let dom = now.getDate();
+	let hours = now.getHours();
+	let minutes = now.getMinutes();
+	let seconds = now.getSeconds();
+
+	let ampm = hours >= 12 ? 'PM' : 'AM';
+	hours = hours % 12;
+	hours = hours ? hours : 12; // the hour '0' should be '12'
+	minutes = minutes < 10 ? '0'+minutes : minutes;
+	if(hours < 10)
+		hours = '0' + hours;
+	if(minutes < 10)
+		minutes = '0' + minutes;
+	if(seconds < 10)
+		seconds = '0' + seconds;
+	
+	let formatDate = dayNames[dow].substr(0, 3) + " " + dom + " " + monthNames[month].substr(0, 3) + " " + hours + ":" + minutes + ":" + seconds + " " + ampm ;
+	if (document.getElementById("datetime")) {
+		document.getElementById("datetime").innerHTML = formatDate;
+	}
+}
+let windowInterval = setInterval(updateTime, 1000);
+
+setTimeout(() => {
+	clearInterval(windowInterval);
+	windowInterval = setInterval(updateTime, 1000);
+}, 1100);
