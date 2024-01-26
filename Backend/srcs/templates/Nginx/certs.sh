3baf1a61ed361d4b4ca3fcb3c3e8d79387cbde36
Radi Aman Riyas <radiriyas@gmail.com>
2024-01-26 22:04:06 +0400

    fix: remove duplicate files from templates/ & add authentication headers to api routes

--- Backend/srcs/templates/Nginx/certs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p /etc/nginx/ssl/;
-openssl req -x509 -newkey rsa:4096 -keyout $SSL_CERTIFICATE_KEY \
-	-out $SSL_CERTIFICATE -sha256 -days 3650 -nodes \
-	-subj "/C=AD/ST=ahassan/L=AD/O=42/OU=NA/CN=pongos42@gmail.com";
-
-sed -i -e "s|SSL_CERTIFICATE|${SSL_CERTIFICATE}|" \
-       -e "s|SSL_KEY|${SSL_CERTIFICATE_KEY}|" /etc/nginx/nginx.conf
