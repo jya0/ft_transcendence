9921d2a6e853bbbfdbadd0f96a79020fc5b6f106
Ahmed Hassan <digiator2020@gmail.com>
2024-01-28 23:50:37 +0400

    auth in login app, rm users app

--- Backend/srcs/login/urls.py
+++ Backend/srcs/login/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.login, name='login'),
-    # path("auth/", views.auth, name="auth"),
-    path('2fa/', views.twoFactor, name='2fa'),
-    path('login/', views.login, name='login'),
-	path('api/logout/', views.logout, name='logout'),
-    # path('register/', views.register_view, name='register'),
+    path("api/auth/", views.auth, name="auth"),
+    path('api/logout/', views.logout, name='logout'),
 ]
