81f65b5f0ce8c83a5b6b11ffab87798079cb71e8
Saim Ali <sali@lab1r6s4.42abudhabi.ae>
2024-02-01 12:42:47 +0400

    fix: pong display name, tournament name check, 2fa button text wrap

--- Frontend/js/utility.js
+++ Frontend/js/utility.js
@@ -26,12 +26,19 @@ export const elementIdEditInnerHTML = (id, innerHTML) => {
 };
 
 export const checkName = (name) => {
+	var usernameRegex = /^[a-zA-Z0-9]+$/;
+
 	if (!name || name.trim().length === 0 || name.trim().length < 5) {
 		loadToast('Display name should not be empty, more than 5 characters and less than 50 characters');
 		return (false);
 	}
-	else if (name.length >= 50) {
-		loadToast('Size of display name should be less than 50 characters');
+	else if (!name.match(usernameRegex)) {
+
+		loadToast('Name should only include alphanumic characters!');
+		return (false);
+	}
+	else if (name.length >= 15) {
+		loadToast('Name should be less then 15 characters');
 		return (false);
 	}
 	return (true);
